a3ab7892ee1600a4b29d5dcfabb011aacbf2b4e8
huynt654 <huynt654@gmail.com>
2025-01-15 03:52:36 +0700

    upload uit

---
+++ uit/cmp/inference.py
@@ -0,0 +1,122 @@
+import os
+from pathlib import Path
+import time
+import datetime
+import argparse
+import json
+import math
+import random
+import numpy as np
+from ruamel.yaml import YAML
+yaml = YAML(typ='safe')
+from prettytable import PrettyTable
+
+import torch
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+from torch.cuda.amp import GradScaler
+from torch.optim import Optimizer
+
+from transformers import BertTokenizer
+
+import utils
+from models.model_search import Search
+
+from dataset import create_dataset, create_sampler, create_loader
+from dataset.search_dataset import TextMaskingGenerator
+from scheduler import create_scheduler
+from optim import create_optimizer
+
+from train import train_model
+from eval import evaluation_itm, mAP, evaluation_itc
+
+
+def main(args, config):
+    utils.init_distributed_mode(args)
+    device = torch.device(args.device)
+    world_size = utils.get_world_size()
+
+    seed = args.seed + utils.get_rank()
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    cudnn.benchmark = True
+
+    print("### Creating model")
+    tokenizer = BertTokenizer.from_pretrained(config['text_encoder'])
+    model = Search(config=config)
+    if config['load_pretrained']:
+        model.load_pretrained(args.checkpoint)
+    model = model.to(device)
+    print("Total Params: ", sum(p.numel() for p in model.parameters() if p.requires_grad))
+
+    model_without_ddp = model
+    if args.distributed:
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+        model_without_ddp = model.module
+
+    print("### Creating search dataset")
+    test_dataset = create_dataset(config, evaluate=False, inference=True)
+
+    print("### Start inferencing")
+    test_loader = create_loader([test_dataset], [None],
+                                batch_size=[config['batch_size_test']],
+                                num_workers=[4],
+                                is_trains=[False],
+                                collate_fns=[None])[0]
+
+    sims_matrix_t2i, image_embeds, text_embeds, text_atts = evaluation_itc(
+        model_without_ddp, data_loader=test_loader, tokenizer=tokenizer, device=device, config=config)
+    
+    sims_matrix_t2i = torch.load(args.beit3_score)
+
+    score_test_t2i = evaluation_itm(model_without_ddp, device, config, args,
+                                    sims_matrix_t2i, image_embeds, text_embeds, text_atts, args.blip2_weight, args.blip2_score, args.blip2_weight, args.clip_score, args.clip_weight)
+
+    similarity = torch.tensor(score_test_t2i)
+    indices = torch.argsort(similarity, dim=1, descending=True)
+    print(indices.shape)
+    print(indices)
+    
+    # Get the top 10 indices
+    top_10_indices = indices[:, :10]
+
+    # Output to a text file
+    with open(f"{args.output_file}", "w") as f:
+        for i, top10 in enumerate(top_10_indices):
+            string = ' '.join([test_dataset.g_pids[id] for id in top10])
+            f.write(f"{string}\n")
+
+    print(f"Top 10 results saved to {args.output_file}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, required=True)
+    parser.add_argument('--task', type=str, required=True)
+    parser.add_argument('--output_dir', type=str, required=True)
+    parser.add_argument('--checkpoint', type=str)
+    parser.add_argument('--bs', default=16, type=int, help="mini batch size")
+    parser.add_argument('--seed', default=42, type=int)
+    parser.add_argument('--evaluate', action='store_true')
+    parser.add_argument('--output_file', type=str)
+    parser.add_argument('--beit3_score',type=str)
+    parser.add_argument('--beit3_weight', default=0.925, type=float)
+    parser.add_argument('--blip2_score', type=str)
+    parser.add_argument('--blip2_weight', default=0.9, type=float)
+    parser.add_argument('--clip_score', type=str)
+    parser.add_argument('--clip_weight', default=0.9, type=float)
+
+    parser.add_argument('--device', default='cuda')
+    parser.add_argument('--world_size', default=1, type=int, help='number of distributed processes')
+    parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
+    parser.add_argument('--distributed', action='store_false')
+    args = parser.parse_args()
+
+    Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+    print("### output_dir:", args.output_dir)
+
+    config = yaml.load(open(args.config, 'r'))
+    yaml.dump(config, open(os.path.join(args.output_dir, 'config.yaml'), 'w'))
+
+    main(args, config)
---
+++ uit/cmp/scheduler.py
@@ -0,0 +1,68 @@
+from torch.optim.lr_scheduler import LambdaLR
+import math
+
+
+def create_scheduler(args, optimizer):
+    if 'num_training_steps' not in args:
+        args['num_training_steps'] = args['epochs'] * args['step_per_epoch']
+    print("### num_training_steps, ", args['num_training_steps'], flush=True)
+
+    if isinstance(args['num_warmup_steps'], float):
+        assert 0 <= args['num_warmup_steps'] < 1
+        args['num_warmup_steps'] = int(args['num_training_steps'] * args['num_warmup_steps'])
+    print("### num_warmup_steps, ", args['num_warmup_steps'], flush=True)
+
+    print('sched:', args.sched, flush=True)
+
+    if args.sched == 'linear':
+        def lr_lambda(current_step: int):
+            if current_step < args.num_warmup_steps:
+                return float(current_step) / float(max(1, args.num_warmup_steps))
+            return max(
+                0.0, float(args.num_training_steps - current_step) / float(
+                    max(1, args.num_training_steps - args.num_warmup_steps))
+            )
+
+        lr_scheduler = LambdaLR(optimizer, lr_lambda, last_epoch=-1)
+
+    elif args.sched == 'step':
+        def lr_lambda(current_step: int):
+            if current_step < args.num_warmup_steps:
+                return float(current_step) / float(max(1, args.num_warmup_steps))
+            elif current_step < args.num_warmup_steps * 4:
+                tt = 1
+            elif current_step < args.num_warmup_steps * 7:
+                tt = 0.5
+            else:
+                tt = 0.2
+
+            return tt * max(
+                0.0, float(args.num_training_steps - current_step) / float(
+                    max(1, args.num_training_steps - args.num_warmup_steps))
+            )
+
+        lr_scheduler = LambdaLR(optimizer, lr_lambda, last_epoch=-1)
+    elif args.sched == 'cosine':
+        print('Load cosine')
+        def lr_lambda(current_step, warmup_steps, total_steps, min_lr=1e-5, max_lr=1e-4):
+            if current_step < warmup_steps:
+                # Warmup phase: tăng lr tuyến tính từ 0 lên max_lr
+                return current_step / warmup_steps
+            else:
+                # Cosine Annealing phase: giảm lr từ max_lr về min_lr
+                progress = (current_step - warmup_steps) / (total_steps - warmup_steps)
+                cosine_decay = 0.5 * (1 + math.cos(math.pi * progress))
+                return (cosine_decay * (max_lr - min_lr) + min_lr) / max_lr
+        warmup_steps = args.num_training_steps * 0.02
+        lr_scheduler = LambdaLR(
+            optimizer, lr_lambda=lambda step: lr_lambda(step, warmup_steps, args.num_training_steps)
+        )
+            
+    else:
+        raise NotImplementedError(f"args.sched == {args.sched}")
+
+    return lr_scheduler
+
+
+# stepLR
+# 
